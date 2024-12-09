67d691f8efc08b19e260d9eae267a1318be0da07
syedz226 <zilanahamed@gmail.com>
2024-12-09 17:43:30 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,16 @@
+def gd(repo)
+  {
+    git 'https://github.com/IntelliqDevops/${repo}.git'
+  }
+def cb()
+  {
+    sh 'mvn package'
+  }
+def cd(jobname,ip,context)
+  {
+    sh 'scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war'
+  }
+def ct(jobname)
+  {
+       sh 'java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar'
+  }
