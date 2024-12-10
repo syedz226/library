eaaed99e188b736c31bc8111ab6e697417b0f99b
syedz226 <zilanahamed@gmail.com>
2024-12-10 11:19:29 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,16 +1,19 @@
 def gd(repo)
-  {
-    git 'https://github.com/IntelliqDevops/${repo}.git'
-  }
+{
+  git 'https://github.com/IntelliqDevops/${repo}.git'
+}
+
 def cb()
-  {
-    sh 'mvn package'
-  }
+{
+  sh 'mvn package'
+}
+
 def cd(jobname,ip,context)
-  {
-    sh 'scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war'
-  }
+{
+  sh 'scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war'
+}
+
 def ct(jobname)
-  {
-       sh 'java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar'
-  }
+{
+   sh 'java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar'
+}
