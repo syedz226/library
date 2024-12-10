7f8dcb3ac8c84829c823b83cbbede8e29f15dee0
syedz226 <zilanahamed@gmail.com>
2024-12-10 12:16:05 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,10 +10,10 @@ def cb()
 
 def cd(jobname,ip,context)
 {
-  sh 'scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war'
+  sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
 
 def ct(jobname)
 {
-   sh 'java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar'
+   sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
 }
